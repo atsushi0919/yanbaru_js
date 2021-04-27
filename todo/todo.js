5685fab4455538f1de00011b2de5dc496d87b39a
atsushi0919 <hatake0919@yahoo.co.jp>
2021-04-27 12:05:59 +0900

    add/todo

---
+++ todo/todo.js
@@ -0,0 +1,58 @@
+const addButton = document.querySelector('.todo-button');
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
+const deleteButton = document.createElement('button');
+
+const addTodo = () => {
+  // 空欄ならタスクを追加しない
+  if (todoInput.value === '') return;
+
+  const newTodo = document.createElement('li');
+
+  // タスク名
+  const todoContent = document.createElement('span');
+  todoContent.innerText = todoInput.value;
+  todoContent.classList.add('todo-content');
+  newTodo.appendChild(todoContent);
+
+  // 完了 未着手ボタン
+  todoList.appendChild(newTodo);
+  const checkButton = document.createElement('button');
+
+  checkButton.addEventListener('click', switchState);
+  checkButton.innerHTML = '□未着手';
+  checkButton.classList.add('check-button');
+  newTodo.appendChild(checkButton);
+
+  // 削除ボタン
+  const deleteButton = document.createElement('button');
+  deleteButton.addEventListener('click', deleteTodo);
+  deleteButton.innerHTML = '削除';
+  deleteButton.classList.add('delete-button');
+  newTodo.appendChild(deleteButton);
+
+  // リストへ追加
+  todoList.appendChild(newTodo);
+
+  // 入力フォームの値を消去
+  todoInput.value = '';
+};
+
+const switchState = (e) => {
+  let checkButton = e.target;
+
+  if (!checkButton.classList.contains('complete')) {
+    checkButton.innerHTML = '完了';
+    checkButton.classList.add('complete');
+  } else {
+    checkButton.innerHTML = '□ 未着手';
+    checkButton.classList.remove('complete');
+  }
+};
+
+const deleteTodo = (e) => {
+  const todoList = e.target.closest('li');
+  todoList.remove();
+};
+
+addButton.addEventListener('click', addTodo);
